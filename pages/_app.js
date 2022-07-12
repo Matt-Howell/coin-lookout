82b1f867e2e8c67c021b45cef74542913ef36f94
Matt Howell <matthewh6010@gmail.com>
2022-07-12 17:53:12 +0100

    git commit -a

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import '../styles/globals.css'
+import '../styles/bootstrap.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { mode } from "@chakra-ui/theme-tools"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function Cl({ Component, pageProps }) {
+  const config = {
+    useSystemColorMode: true,
+    initialColorMode: "dark",
+    styles: {
+      Global: () => ({
+        body: {
+          bg: mode('#fffff', 'gray.800')
+        },
+      }),
+    }
+};
+  
+  const customTheme = extendTheme({ config })
+
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
-export default MyApp
+export default Cl
